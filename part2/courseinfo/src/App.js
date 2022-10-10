b959f00b623a63947422842ddd19b5c383a81b16
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-10-10 16:27:48 +0200

    2a - Exercises 2.1.-2.5. done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -3,8 +3,8 @@ import Course from "./components/Course";
 const App = () => {
   const courses = [
     {
-      id: 1,
       name: "Half Stack application development",
+      id: 1,
       parts: [
         {
           name: "Fundamentals of React",
@@ -25,6 +25,22 @@ const App = () => {
           name: "Redux",
           exercises: 11,
           id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
         },
       ],
     },
